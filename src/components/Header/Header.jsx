e81617ee1b363099420ee54a6ffe6f0bf240bea0
Matute95 <114253969+Matute95@users.noreply.github.com>
2023-07-03 20:13:49 -0400

    2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef } from "react";
 
 import { Container } from "reactstrap";
 import logo from "../../assets/images/res-logo.png";
